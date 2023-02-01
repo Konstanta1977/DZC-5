4b517b62280ccb80afbda66ed3ca47932e239599
Nata Konstanta <rio201150@gmail.com>
2023-02-01 15:04:58 +0500

    задача 38

--- Program.cs
+++ Program.cs
@@ -41,7 +41,9 @@
 // }
 // Console.WriteLine("Введите значение количество элементов массива");
 // int size = Convert.ToInt32(Console.ReadLine());
-
+// int [] numbers = new int[size];
+// FillArrayRandomNumbers(numbers);
+// System.Console.WriteLine();
 // if (size < 1)
 // {
 //     Console.WriteLine("Некорректное значение");
@@ -56,3 +58,31 @@
 //     Console.WriteLine("Сумма элементов, стоящих на нечётных позициях: " + summOddIndicies(arr));
 // }
 
+// Задача 38: Задайте массив вещественных чисел. Найдите разницу между максимальным и минимальным элементов массива.
+
+// void enterArrayElement(double[] arr, int index)
+// {
+//     Console.WriteLine("Введите значение элемента на позиции " + (index + 1));
+//     arr[index] = Convert.ToDouble(Console.ReadLine());
+// }
+// Console.WriteLine("Введите значение количество элементов массива");
+// int size = Convert.ToInt32(Console.ReadLine());
+// double[] arr = new double[size];
+// for (int index = 0; index < size; index++)
+// {
+//     enterArrayElement(arr, index);
+// }
+// double min = Double.MaxValue;
+// double max = Double.MinValue;
+// foreach (var number in arr)
+// {
+//     if (number < min)
+//     {
+//         min = number;
+//     }
+//     if (number > max)
+//     {
+//         max = number;
+//     }
+// }
+// Console.WriteLine("Разницу между максимальным и минимальным элементом массива: " + (max - min));
